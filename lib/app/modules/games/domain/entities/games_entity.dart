06e3e2f7b9be12501c3c936edaf1ddd656500e05
Carlos Oliveira <72700247+CarlosOliveiraa@users.noreply.github.com>
2022-03-22 15:21:22 -0300

    Iniciando os trabalhos

---
+++ lib/app/modules/games/domain/entities/games_entity.dart
@@ -0,0 +1,15 @@
+class GamesEntity {
+  final int id;
+  final String title;
+  final String thumbnail;
+  final String gnre;
+  final String platform;
+
+  GamesEntity({
+    required this.id,
+    required this.title,
+    required this.thumbnail,
+    required this.gnre,
+    required this.platform,
+  });
+}
